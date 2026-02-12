bd42bd74aff3db258fd0db7afb5c8de2e97b2c88
Pau Guerrero <pauguerrero@gmail.com>
2026-02-12 16:02:44 +0100

    add file server for static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,14 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// register the file server as the handler for all URL paths
+	// starting with "/static/". We strip the "/static" before
+	// the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	// register other app routes
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
