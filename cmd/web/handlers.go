8251f5642fe5c4d9d394061bfce5da07bcf6c5cf
Pau Guerrero <pauguerrero@gmail.com>
2026-02-17 13:00:23 +0100

    show snippets in home

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
 
@@ -17,28 +16,38 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// init a slice containing path to files
-	files := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/partials/nav.tmpl",
-		"./ui/html/pages/home.tmpl",
-	}
-
-	// read template file into a template set
-	// passing the slice as a variadic parameter
-	ts, err := template.ParseFiles(files...)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
-		app.serverError(w, err) // user serverError() helper
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
 		return
 	}
 
-	// write the template content as the response body
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, err) // use serverError() helper
-		http.Error(w, "Internal server error", 500)
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
+
+	// init a slice containing path to files
+	// files := []string{
+	// 	"./ui/html/base.tmpl",
+	// 	"./ui/html/partials/nav.tmpl",
+	// 	"./ui/html/pages/home.tmpl",
+	// }
+	//
+	// read template file into a template set
+	// passing the slice as a variadic parameter
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, err) // user serverError() helper
+	// 	http.Error(w, "Internal Server Error", 500)
+	// 	return
+	// }
+	//
+	// write the template content as the response body
+	// err = ts.ExecuteTemplate(w, "base", nil)
+	// if err != nil {
+	// 	app.serverError(w, err) // use serverError() helper
+	// 	http.Error(w, "Internal server error", 500)
+	// }
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
