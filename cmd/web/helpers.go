a70adb1f51ba5b1e91b49a0274751761dc50cc3b
Pau Guerrero <pauguerrero@gmail.com>
2026-02-19 14:07:44 +0100

    remove superfluous line

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -11,8 +11,6 @@ import (
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errorLog.Output(2, trace)
-
-	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
 // clientError sends a specific status code & description to the user
