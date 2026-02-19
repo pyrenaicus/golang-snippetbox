ba5f6289b15a1bc15fbf4815dca6bbc60ca80b45
Pau Guerrero <pauguerrero@gmail.com>
2026-02-19 11:11:58 +0100

    create in-memory map to cache parsed templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,10 +1,48 @@
 package main
 
-import "snippetbox.cnoua.org/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"snippetbox.cnoua.org/internal/models"
+)
 
 // define a templateData type to act as the holding structure for
 // any dynamic data passed to our html templates.
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	// initialize a new map to act as the cache
+	cache := map[string]*template.Template{}
+	// get a slice of all filepaths for 'page' templates
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		// extract the file name from the full filepath
+		name := filepath.Base(page)
+
+		// create a slice containing the filepaths for our base template, partials and page
+		files := []string{
+			"./ui/html/base.tmpl",
+			"./ui/html/partials/nav.tmpl",
+			page,
+		}
+
+		// parse files into a template set
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add template set to the map, using page name as the key
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
