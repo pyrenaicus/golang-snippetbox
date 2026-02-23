07f79f37b86e0e45ef63b34e0eef5da83d80921b
Pau Guerrero <pauguerrero@gmail.com>
2026-02-23 16:07:21 +0100

    create middleware to recover from panic

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -20,6 +21,23 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// create a deferred fn which will always be run in the event of a panic
+		// as Go unwinds the stack
+		defer func() {
+			// use the built-in recover fn to check if there's been a panic or not
+			if err := recover(); err != nil {
+				// set a "Connection: close" header in the response
+				w.Header().Set("Connection", "close")
+				// return a 500 error
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
 		next.ServeHTTP(w, r)
 	})
 }
