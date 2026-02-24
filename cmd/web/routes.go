da317a71e8e668b731b595fe1f78ed79fa20d9dd
Pau Guerrero <pauguerrero@gmail.com>
2026-02-24 10:00:27 +0100

    update routes to use httprouter

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,19 +3,23 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 // it returns a http.Handler instead of *http.ServeMux
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
+	// initialize the router
+	router := httprouter.New()
 
+	// route for the static files
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	// create a middleware chain used for every request
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
