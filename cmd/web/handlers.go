5652b352bc8112282f2d3a3f7770064bba14f181
Pau Guerrero <pauguerrero@gmail.com>
2026-02-24 15:59:49 +0100

    validate form data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/julienschmidt/httprouter"
 	"snippetbox.cnoua.org/internal/models"
@@ -65,9 +67,51 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly!\n\n- Kobayashi Issa"
-	expires := 7
+	// call r.ParseForm() which adds any data in POST request body to
+	// r.PostForm map.
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	// use r.PostForm.Get() to retrieve the title and content
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+
+	// r.PostForm.Get() always return form data as a string, however we expect
+	// expires value to be an integer
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// initialize a map to hold any validation errors for the form fields
+	fieldErrors := make(map[string]string)
+
+	// check title is not blank and not more than 100 char long, if it fails
+	// add a message to the errors map using field name as key
+	if strings.TrimSpace(title) == "" {
+		fieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		fieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+
+	// chack that content isn't blank
+	if strings.TrimSpace(content) == "" {
+		fieldErrors["content"] = "This field cannot be blank"
+	}
+
+	// check expires value matches one of the permitted values
+	if expires != 1 && expires != 7 && expires != 365 {
+		fieldErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
+
+	// if there are any errors, dump them in a plain text response and return
+	if len(fieldErrors) > 0 {
+		fmt.Fprint(w, fieldErrors)
+		return
+	}
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
