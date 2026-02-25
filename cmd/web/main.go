36a0c1cb8e21533a18158954e8779017a65b354b
Pau Guerrero <pauguerrero@gmail.com>
2026-02-25 15:47:00 +0100

    establish a session manager

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,20 +7,24 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	// import our models package
 	"snippetbox.cnoua.org/internal/models"
 
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql" // alias package name to the blank identifier
 )
 
 type application struct {
-	errorLog      *log.Logger
-	infoLog       *log.Logger
-	snippets      *models.SnippetModel
-	templateCache map[string]*template.Template
-	formDecoder   *form.Decoder
+	errorLog       *log.Logger
+	infoLog        *log.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
 }
 
 func main() {
@@ -50,13 +54,20 @@ func main() {
 	// initialize a decoder instance
 	formDecoder := form.NewDecoder()
 
+	// initialize a session manager, configure it to use mysql as session store
+	// set a lifetime of 12 hours (session expire)
+	sessionManager := scs.New()
+	sessionManager.Store = mysqlstore.New(db)
+	sessionManager.Lifetime = 12 * time.Hour
+
 	// initialize a models.SnippetModel instance and add it to the application dependencies
 	app := &application{
-		errorLog:      errorLog,
-		infoLog:       infoLog,
-		snippets:      &models.SnippetModel{DB: db},
-		templateCache: templateCache,
-		formDecoder:   formDecoder,
+		errorLog:       errorLog,
+		infoLog:        infoLog,
+		snippets:       &models.SnippetModel{DB: db},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManager,
 	}
 
 	srv := &http.Server{
