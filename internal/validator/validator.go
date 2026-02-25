f9c066c214ab6560404c0ec9d0474a52e937100f
Pau Guerrero <pauguerrero@gmail.com>
2026-02-25 11:36:08 +0100

    create validator package

---
+++ internal/validator/validator.go
@@ -0,0 +1,57 @@
+package validator
+
+import (
+	"strings"
+	"unicode/utf8"
+)
+
+// define a Validator type containing a map of validation errors
+type Validator struct {
+	FieldErrors map[string]string
+}
+
+// Valid() returns true if FieldErrors map contains no errors
+func (v *Validator) Valid() bool {
+	return len(v.FieldErrors) == 0
+}
+
+// AddFieldError() adds an error message to FieldErrors map as long
+// as no entry already exists for given key
+func (v *Validator) AddFieldError(key, message string) {
+	// initialize map if it isn't already initialized
+	if v.FieldErrors == nil {
+		v.FieldErrors = make(map[string]string)
+	}
+
+	if _, exists := v.FieldErrors[key]; !exists {
+		v.FieldErrors[key] = message
+	}
+}
+
+// CheckField() adds an error message to FieldErrors only if
+// a validation check is not ok
+func (v *Validator) CheckField(ok bool, key, message string) {
+	if !ok {
+		v.AddFieldError(key, message)
+	}
+}
+
+// NotBlank() returns true if a value is not an empty string.
+func NotBlank(value string) bool {
+	return strings.TrimSpace(value) != ""
+}
+
+// MaxChars() returns true if a value contains no more than n characters.
+func MaxChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) <= n
+}
+
+// PermittedInt() returns true if a value is in a list of permitted characters.
+func PermittedInt(value int, permittedValues ...int) bool {
+	for i := range permittedValues {
+		if value == permittedValues[i] {
+			return true
+		}
+	}
+	return false
+}
