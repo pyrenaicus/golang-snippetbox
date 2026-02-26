4d53c0889e36494dde96f3bac2769771d9e39395
Pau Guerrero <pauguerrero@gmail.com>
2026-02-26 09:05:41 +0100

    create a new dynamic middleware chain

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,10 +20,18 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	router.HandlerFunc(http.MethodGet, "/", app.home)
-	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
-	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
-	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
+	// middleware chain containing the middleware specific to dynamic
+	// application routes
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
+
+	// update routes to use the new dynamic middleware chain followed by the
+	// appropriate handler fn. Note that because the alice ThenFunc() returns
+	// a http.Handler (rather than a http.HandlerFunc) we also need to switch
+	// to registering the route using the router.Handler() method.
+	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
+	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
+	router.Handler(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.snippetCreate))
+	router.Handler(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
 	// create a middleware chain used for every request
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
