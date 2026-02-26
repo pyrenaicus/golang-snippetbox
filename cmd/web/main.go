9635cbddeadd40ecba4c465b90fbb42a7476987f
Pau Guerrero <pauguerrero@gmail.com>
2026-02-26 12:58:28 +0100

    non-default tls settings; restrict elliptic curves to be used

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"html/template"
@@ -72,10 +73,17 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
+	// init a struct to hold non-default TLS settings. We change the curve
+	// preferences value, so that only elliptic curves with assembly
+	// implementation are used.
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
 	srv := &http.Server{
-		Addr:     *addr,
-		ErrorLog: errorLog,
-		Handler:  app.routes(),
+		Addr:      *addr,
+		ErrorLog:  errorLog,
+		Handler:   app.routes(),
+		TLSConfig: tlsConfig,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
