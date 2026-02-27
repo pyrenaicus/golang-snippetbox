6ac129f8625b2bab586d0bb2d617dcf3566630cc
Pau Guerrero <pauguerrero@gmail.com>
2026-02-27 14:25:19 +0100

    update userLogin handler to render login page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -188,7 +188,9 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Display a HTML form for logging in a user...")
+	data := app.newTemplateData(r)
+	data.Form = userLoginForm{}
+	app.render(w, http.StatusOK, "login.tmpl", data)
 }
 
 func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
