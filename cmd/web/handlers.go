351e79f904c0a03c7d0f5917b16bde3b6d0ad2e9
Pau Guerrero <pauguerrero@gmail.com>
2026-02-27 11:52:33 +0100

    run validation checks on user signup form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -135,6 +135,31 @@ func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+	// declare a zero-valued instance of userSignupForm struct
+	var form userSignupForm
+	// parse the data into it
+	err := app.decodePostForm(r, &form)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	// validate form contents
+	form.CheckField(validator.NotBlank(form.Name), "name", "This field cannot be blank")
+	form.CheckField(validator.NotBlank(form.Email), "email", "This field cannot be blank")
+	form.CheckField(validator.Matches(form.Email, validator.EmailRX), "email", "This field must be a valid email address")
+	form.CheckField(validator.NotBlank(form.Password), "password", "This field cannot be blank")
+	form.CheckField(validator.MinChars(form.Password, 8), "password", "This field must be at least 8 characters long")
+
+	// if errors redisplay signup form along a 422 status code
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "signup.tmpl", data)
+		return
+	}
+
+	// otherwise send the placeholder response for now
 	fmt.Fprintln(w, "Create a new user...")
 }
 
