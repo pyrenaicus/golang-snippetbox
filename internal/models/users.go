38c2088bd3410f01f52586aa2b1253b156876dcd
Pau Guerrero <pauguerrero@gmail.com>
2026-02-27 12:48:49 +0100

    update users model

--- internal/models/users.go
+++ internal/models/users.go
@@ -2,7 +2,12 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"strings"
 	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -19,6 +24,30 @@ type UserModel struct {
 
 // Insert adds a new record to the users table
 func (m *UserModel) Insert(name, email, password string) error {
+	// create a bcrypt hash of the password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+	VALUES(?, ?, ?, UTC_TIMESTAMP())`
+
+	// insert into users table
+	_, err := m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		// if error, check wether the error has type *mysql.MySQLError, if it does,
+		// assign it to mySQLError variable. Then check if it relates to our
+		// users_uc_email key by checking error code equals 1062 and contents
+		// of error message string. If it does, return ErrDuplicateEmail error
+		var mySQLError *mysql.MySQLError
+		if errors.As(err, &mySQLError) {
+			if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message, "users_uc_email") {
+				return ErrDuplicateEmail
+			}
+		}
+		return err
+	}
 	return nil
 }
 
