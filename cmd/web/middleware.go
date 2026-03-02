fe6c9835aec6771b2dec32d6739319847ed76f8e
Pau Guerrero <pauguerrero@gmail.com>
2026-03-02 16:15:48 +0100

    create an authenticate() middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
@@ -74,4 +75,35 @@ func noSurf(next http.Handler) http.Handler {
 	})
 
 	return csrfHandler
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// retrieve the authenticatedUserID from the session. This will return
+		// 0 if no value is in the session, in which case we call the next handler
+		// in the chain as normal and return.
+		id := app.sessionManager.GetInt(r.Context(), "authenticatedUserID")
+		if id == 0 {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		// otherwise, we check to see if a user with that ID exists in our db
+		exists, err := app.users.Exists(id)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+		// if auser is found we know the request is coming from an authenticated
+		// user. We create a new copy of the request with an isAuthenticatedContextKey
+		// value of true in the request context and assign it to r
+		if exists {
+			ctx := context.WithValue(r.Context(), isAuthenticatedContextKey, true)
+			r = r.WithContext(ctx)
+		}
+
+		// call the next handler in the chain
+		next.ServeHTTP(w, r)
+	})
 }
